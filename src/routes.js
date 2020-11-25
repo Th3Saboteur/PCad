065c6ba3b8382b39e0c94acbcf462587464302b2
Th3Saboteur <batistabruno.bbs@outlook.com>
2020-11-25 17:02:52 -0300

    Segundo Commit

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,17 @@
 const express = require('express'); //Importa express
 const routes = express.Router(); //Extrai apenas as rotas do express
-const connection = require('./database/connection'); //Importa script de conexão com o banco
-//const productCtrl = require('./controller/productCtrl'); //Importa controlador para acesso ao banco (script)
-const crypto = require('crypto'); //Pacote do Node 
+const productCtrl = require('./controller/productCtrl'); //Importa controlador para acesso ao banco (script)
 
-routes.get('/', (req, res)=>{
-    res.send("Salve, truta! Tudo suave até aqui...");
-});
+routes.get('/', productCtrl.test);
 
-routes.post('/createproduct', async(req, res)=>{
-    const {tipo, marca, modelo} = req.body; //Atribui o que vem da requisição às variáveis esperadas
-    const id = crypto.randomBytes(4).toString('hex'); //Gera arquivo de 4 bytes e transforma em string no formato hexadecimal
-    await connection('products').insert({ //Insere na tabela products os dados obtidos
-        id, 
-        tipo, 
-        marca, 
-        modelo
-    });
-    //console.log(params);
-    res.json({id}); //Responde com o id (como objeto) caso dê tudo certo
-}); 
+routes.post('/products/create', productCtrl.createProduct); //Chama a função do controlador para a rota 
 
-routes.get('/products', async(req, res)=>{
-    const users = await connection('products').select('*'); //Seleciona todas tuplas da tabela products
-    //console.log(params);
-    res.json(users); //Responde com os usuários
-}); 
+routes.get('/products/list', productCtrl.listProducts); //Chama a função do controlador para a rota    
 
-// routes.get('/users', "Salve, truta");
+routes.get('/products/:id', productCtrl.showProduct); //Chama a função do controlador para a rota  
+
+routes.put('/products/:id', productCtrl.updateProduct); //Chama a função do controlador para a rota 
+
+routes.delete('/products/:id', productCtrl.deleteProduct); //Chama a função do controlador para a rota 
 
 module.exports = routes; //Exporta variável routes (com as rotas) para acesso externo
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express'); //Importa framework express 
 const routes = require('./routes'); //Importa script routes.js
-//const cors = require('cors'); //Importa API cors (gerenciamento de domínios)
+const cors = require('cors'); //Importa API cors (gerenciamento de domínios)
 
-const app = express(); //Inicializa aplicação usando express
+const app = express(); //Inicializa aplicação usando express (sem limite de domínios)
 
-//app.use(cors());
+app.use(cors()); //Inicia o cors com acesso para qualquer requisição
 app.use(express.json()); //Permite que a aplicação reconheça o fomato json (nas requisições)
 app.use(routes); //Deve ser depois de "app.use(express.json());" para renhecer as requisições
 
